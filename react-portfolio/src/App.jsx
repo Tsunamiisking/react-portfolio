2fd31e4a8b66d37e9bdd7412cb9222c13d85ef41
Allen Douglas <allenissabigboi@gmail.com>
2024-10-05 16:32:46 -0700

    Added and configured tailwind

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
-
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import MainPage from './pages/MainPage';
 
 function App() {
 
 
-  return (
-    <>
-    </>
-  )
-}
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={< MainPage />}>
+      </Route>
+    )
+  );
+  return <RouterProvider router={router} />;
+};
 
 export default App
