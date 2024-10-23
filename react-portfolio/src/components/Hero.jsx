2a3683eff5f7262f8e62d3a537a6075b3d64c60a
Allen Douglas <allenissabigboi@gmail.com>
2024-10-23 16:51:00 -0700

    made changes

--- react-portfolio/src/components/Hero.jsx
+++ react-portfolio/src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         Hello World!
     </Intro>
     <section>
-        <div className="font-roboto grid grid-cols-2 w-11/12 border-2 border-lightcoral ml-auto mr-auto">
+        <div className=" rounded-t font-roboto grid grid-cols-2 w-11/12 border-2 border-lightcoral ml-auto mr-auto">
           <div className="grid grid-cols-2 justify-between col-span-2 border-2 border-lightcoral pt-2 pb-2 ">
             <div className="nav-left flex items-center content-center pl-2">
               <div className="mr-1 w-4 h-4 bg-red-500 rounded-full"></div>
